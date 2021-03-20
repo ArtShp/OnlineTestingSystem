ea06e15b39666d31a4ca10e4ea68805acee36969
Артём <56469391+ArtShp@users.noreply.github.com>
2021-03-20 23:13:20 +0300

    Rebuilding models Adding parrams to classes

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+"""
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -16,3 +17,40 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+"""
+
+################################################################################################
+
+
+class Quiz(models.Model):
+    #general
+    title = models.CharField()
+    description = models.CharField()
+    figure = models.ImageField()
+    type = models.CharField()
+    category = models.CharField()
+    users = None
+
+    #time
+    start_together = models.BooleanField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    time_limited = models.BooleanField()
+    time_to_do = models.DurationField()
+
+    #other
+    random_order = models.BooleanField()
+    show_result_mode = models.DecimalField() #0-instantly, 1-check button after all, 2-after the end
+
+    check_auto = models.BooleanField()
+
+
+
+class Question(models.Model):
+    quiz = models.OneToOneField()
+    figure = models.ImageField()
+    content = models.CharField()
+    tip = models.CharField()
+    answers = None
+
