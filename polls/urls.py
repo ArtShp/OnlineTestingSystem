59d2e0916f12d7712a3cac894f4916c59fb00a29
Артём <56469391+ArtShp@users.noreply.github.com>
2021-03-25 11:08:00 +0300

    Updating views Deleting trash(old files)

--- polls/urls.py
+++ polls/urls.py
@@ -2,15 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
-"""
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'), #ex: /polls/
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'), #ex: /polls/5
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), #ex: /polls/5/results
-    path('<int:question_id>/vote/', views.vote, name='vote'), #ex: /polls/5/vote/
-]
-"""
 
 urlpatterns = [
-    path('<int:quiz_id>/', views.show_quiz, name='test'),
+    path('', views.show_all_quiz, name='show_all_quiz'),
+    path('<int:quiz_id>/', views.show_quiz, name='show_quiz'),
 ]
