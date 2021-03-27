5f5f4ae42fe826b99c4fd09dd399a443a435d5e6
Артём <56469391+ArtShp@users.noreply.github.com>
2021-03-27 14:19:18 +0300

    Updating models

--- polls/models.py
+++ polls/models.py
@@ -2,64 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 # HELP TEXT
-
-class Quiz(models.Model):
-    title = models.CharField(verbose_name='Название', max_length=100, blank=False)
-    pub_date = models.DateTimeField(verbose_name='Дата публикации', default=timezone.now)
-    description = models.CharField(verbose_name='Описание', max_length=200, blank=True)
-
-    # figure = models.ImageField()
-    # type = models.CharField(max_length=50, blank=True)
-
-    # create class Category for Quiz and Question
-    # category = models.CharField(max_length=50, blank=True)
-
-    # attempts_limited = models.BooleanField()
-    # max 999 attempts
-    # attempts = models.DecimalField(max_digits=3, decimal_places=0)
-
-    # subject = None
-    # author = None # create class
-
-    # quiz_creator can choose: student(s), class(es), parallel(s), all(school)
-    # users = None # create class
-
-    """
-    time
-    
-    start_together = models.BooleanField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
-    time_limited = models.BooleanField()
-    time_to_do = models.DurationField()
-    """
-
-    random_order = models.BooleanField(verbose_name='Перемешать вопросы')
-
-    # 0-instantly, 1-check button after all, 2-after the end
-    # show_result_mode = models.DecimalField(max_digits=1, decimal_places=0)
-
-    def __str__(self):
-        return self.title
-
-
-class Question(models.Model):
-    quiz = models.ForeignKey(Quiz, verbose_name='Тест', on_delete=models.CASCADE)
-    content = models.CharField(verbose_name='Вопрос', max_length=300, blank=False)
-
-    # figure = models.ImageField()
-    # tip = models.CharField(max_length=100, blank=True)
-
-    # quiz_creator can choose: one from many, many from many, field(s) to fill
-    # answers = None # create class
-
-    def __str__(self):
-        return self.content
-
+# On_delete
 
 class Category(models.Model):
-    pass
+    category_name = models.CharField(verbose_name='Категория', max_length=100, blank=True)
 
 
 class Subject(models.Model):
@@ -80,9 +26,57 @@ class Student(models.Model):
     surname = models.CharField(verbose_name='Фамилия', max_length=50, blank=False)
     # patronymic = father name
     patronymic = models.CharField(verbose_name='Отчество', max_length=50, blank=False)
+    # photo = models.ImageField()
     class_number = models.CharField(verbose_name='Номер класса', max_length=2, blank=False)
     class_letter = models.CharField(verbose_name='Буква класса', max_length=2, blank=False)
 
 
+class Quiz(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=100, blank=False)
+    pub_date = models.DateTimeField(verbose_name='Дата публикации', default=timezone.now)
+    description = models.CharField(verbose_name='Описание', max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='Категория')
+    # Доработать: создателем может быть не только учитель, надо это как-то наследовать
+    creator = models.ForeignKey(Teacher, verbose_name='Создатель')
+    subjects = creator.subjects
+    # Доработать: должно работать иерархическим списком
+    students = models.ManyToManyField(Student, verbose_name='Ученик(и)')
+
+    # figure = models.ImageField()
+    # type = models.CharField(max_length=50, blank=True)
+
+    # attempts_limited = models.BooleanField()
+    # max 999 attempts
+    # attempts = models.DecimalField(max_digits=3, decimal_places=0)
+
+    """
+    time
+    
+    start_together = models.BooleanField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    time_limited = models.BooleanField()
+    time_to_do = models.DurationField()
+    """
+
+    random_order = models.BooleanField(verbose_name='Перемешать вопросы')
+
+    # 0-instantly, 1-check button after all, 2-after the end
+    # show_result_mode = models.DecimalField(max_digits=1, decimal_places=0)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, verbose_name='Тест', on_delete=models.CASCADE)
+    content = models.CharField(verbose_name='Вопрос', max_length=300, blank=False)
+    category = models.ForeignKey(Category, verbose_name='Категория')
+
+    # figure = models.ImageField()
+    # tip = models.CharField(max_length=100, blank=True)
+
+
 class Answer(models.Model):
-    pass
+    question = models.ForeignKey(Question, verbose_name='Вопрос')
+    content = models.CharField(verbose_name='Ответ', max_length=50, blank=False)
+    # Доработать: могут ввести несколько правильных ответов
+    is_correct = models.BooleanField(verbose_name='Ответ правильный?')
