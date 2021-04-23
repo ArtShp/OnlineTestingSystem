63c6f46c559e76d6b699b3c1220552dc991089cc
Артём <56469391+ArtShp@users.noreply.github.com>
2021-04-23 19:19:18 +0300

    Fixing fucking bugs with db)

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Quiz, Question, Answer
+from .models import Quiz, Question, Answer, AnsweredQuiz
 
 '''
 class PersonAdmin(admin.ModelAdmin):
@@ -9,6 +9,7 @@ class PersonAdmin(admin.ModelAdmin):
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(AnsweredQuiz)
 
 '''
 class QuizAdmin(admin.ModelAdmin):
